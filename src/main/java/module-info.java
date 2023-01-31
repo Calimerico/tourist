174605e87465edbf42480693b697387507aec607
spasojepetronijevic <spasojepetronijevic@gmail.com>
2023-01-31 12:24:36 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.tourist.tourist {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.tourist.tourist to javafx.fxml;
+    exports com.tourist.tourist;
+}
